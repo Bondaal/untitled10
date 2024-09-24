e86321589ce49c2875559eb901a778981d722256
Bondaal <160157215+Bondaal@users.noreply.github.com>
2024-09-24 16:46:51 +0300

    Add files via upload

---
+++ src/main/java/ru/netology/javaqa/Post.java
@@ -0,0 +1,17 @@
+package ru.netology;
+
+import ru.netology.javaqa.FormDate;
+
+public class Post {
+    public String name; // поле для имени
+    public String passport;
+
+    public String patronymic;
+
+    public String phone;
+
+    public String surname;
+    public FormDate birthday;
+
+    public boolean subscription;
+}
